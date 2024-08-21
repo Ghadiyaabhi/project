531f8e142e89c976e4f02141cfa4b6b03d7e72fb
Ghadiyaabhi <ghadiyaabhi555@gmail.com>
2024-08-21 14:39:57 +0530

    change

--- src/student/views.py
+++ src/student/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
-from .utils import get_tokens_for_user
+from .utils import get_tokens_for_user # type: ignore
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.models import Token
 from .serializer import forgotpasswordSerializer
