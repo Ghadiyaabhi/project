7d87241cbe5fef20f7408d0d18b9392b9b378285
Ghadiyaabhi <ghadiyaabhi555@gmail.com>
2024-08-21 14:33:28 +0530

    delete

--- src/student/views.py
+++ src/student/views.py
@@ -1,15 +1,3 @@
-from django.shortcuts import render
-from models import Students
-from serializer import StudentsSerializer
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
 
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class StudentCreateView(APIView):
-    def post(self, request):
-        serializer = StudentsSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
