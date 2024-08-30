ec07492036b107fca03eca3a865ac063df6d596d
Ghadiyaabhi <ghadiyaabhi555@gmail.com>
2024-08-30 11:35:52 +0530

    commit

--- src/student/serializer.py
+++ src/student/serializer.py
@@ -2,8 +2,8 @@ from rest_framework import serializers
 from .models import Students
 
 
-class Students(serializers.Modelseriallizer):
+class StudentsSerializer(serializers.ModelSerializer):  
     class Meta:
         model = Students
-        fields = '__all__'
+        fields = '__all__' 
         
